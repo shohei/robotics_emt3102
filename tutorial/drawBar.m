da6c6d45a6f6f602a175246caff6ac8814dfb129
Shohei Aoki <shoaok@gmail.com>
2019-10-07 15:26:10 +0300

    initial commit

---
+++ tutorial/drawBar.m
@@ -0,0 +1,7 @@
+function drawBar
+ clear all; close all;
+ plot3([2,3],[1,4],[1,2],'linewidth',5);
+ grid on;
+ xlabel('x');ylabel('y');zlabel('z');
+ axis([0,5,0,5,0,5]);
+end
---
+++ tutorial/drawRect.m
@@ -0,0 +1,5 @@
+function drawRect
+ clear all; close all;
+ plot([0 1 1 0 0],[0 0 1 1 0],'linewidth',5);
+ xlabel('x');ylabel('y');axis([-2,3,-2,3]);
+end
